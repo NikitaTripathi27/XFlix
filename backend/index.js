0087178d4cd2a046b6145380dd9094487ff1d797
Nikita <124888350+NikitaTripathi27@users.noreply.github.com>
2023-07-22 11:58:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const app =require('./app')
 const mongoose = require("mongoose");
 const port = 8082;
-// const url = "mongodb://127.0.0.1:27017/xflix";
-const url = "mongodb+srv://nikita2399020:8anrV73xWNMtaOy0@xflix.dzktoxy.mongodb.net/?retryWrites=true&w=majority"
+ const url = "mongodb+srv://nikita2399020:8anrV73xWNMtaOy0@xflix.dzktoxy.mongodb.net/?retryWrites=true&w=majority";
+//const url = "mongodb+srv://nikita2399020:8anrV73xWNMtaOy0@xflix.dzktoxy.mongodb.net/?retryWrites=true&w=majority"
 const config = require('./config/config')
 mongoose
-  .connect(config.mongoose.url)
+  .connect(url)
   .then(() => {
     console.log("connected successfully", url);
   })
@@ -13,7 +13,8 @@ mongoose
     console.log("login failed", error);
   });
 
-app.listen(config.port, () => {
-  console.log("connected on port", config.port);
+app.listen(port, () => {
+  console.log("connected on port",port);
 });
+
 
